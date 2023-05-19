d8804e06c0a89283f1450c8d6f662e441f685f18
monosans <hsyqixco@protonmail.com>
2023-05-19 23:51:16 +0300

    Reformat start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel && python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt && python3 -m nitro_generator_checker
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check pip setuptools wheel &&
+python3 -m pip install -U --no-cache-dir --disable-pip-version-check -r requirements.txt &&
+python3 -m nitro_generator_checker
