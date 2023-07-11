d14af7837973e014eeedc1f88d503631c1e5423d
monosans <hsyqixco@protonmail.com>
2023-07-11 07:55:27 +0000

    Update .pre-commit-config.yaml (#26)
    * Update .pre-commit-config.yaml
    
    * Reformat
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- nitro_generator_checker/validators.py
+++ nitro_generator_checker/validators.py
@@ -17,11 +17,9 @@ def max_connections(value: int) -> int:
     if not max_supported or value <= max_supported:
         return value
     logger.warning(
-        (
-            "MaxConnections value is too high. "
-            "Your OS supports a maximum of %d. "
-            "The config value will be ignored and %d will be used."
-        ),
+        "MaxConnections value is too high. "
+        "Your OS supports a maximum of %d. "
+        "The config value will be ignored and %d will be used.",
         max_supported,
         max_supported,
     )
