ef0bb961bc66b21ab2465a8879b64a728da3e79c
monosans <hsyqixco@protonmail.com>
2023-11-24 19:53:58 +0300

    Update User-Agent

--- nitro_generator_checker/nitro_checker.py
+++ nitro_generator_checker/nitro_checker.py
@@ -21,8 +21,8 @@ logger = logging.getLogger(__name__)
 HEADERS = MappingProxyType(
     {
         "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; rv:109.0)"
-            " Gecko/20100101 Firefox/119.0"
+            "Mozilla/5.0 (Windows NT 10.0; rv:120.0)"
+            " Gecko/20100101 Firefox/120.0"
         )
     }
 )
