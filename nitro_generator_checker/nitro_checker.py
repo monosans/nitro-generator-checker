fcf0319e20adb6298ea018679cef9facdd1095a9
monosans <hsyqixco@protonmail.com>
2023-12-26 10:09:24 +0300

    Update User-Agent

--- nitro_generator_checker/nitro_checker.py
+++ nitro_generator_checker/nitro_checker.py
@@ -20,7 +20,7 @@ from .utils import create_background_task
 logger = logging.getLogger(__name__)
 HEADERS = MappingProxyType({
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; rv:120.0) Gecko/20100101 Firefox/120.0"
+        "Mozilla/5.0 (Windows NT 10.0; rv:121.0) Gecko/20100101 Firefox/121.0"
     )
 })
 
