e196a7c429e9a5de7c9acf8e7366e2ed247ceaa8
monosans <hsyqixco@protonmail.com>
2024-01-25 14:52:05 +0300

    Refactor start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-python3 -m venv --upgrade-deps .venv &&
-.venv/bin/python3 -m pip install -U --disable-pip-version-check -r requirements.txt &&
+
+set -eu
+
+python3 -m venv --upgrade-deps .venv
+.venv/bin/python3 -m pip install -U --disable-pip-version-check -r requirements.txt
 .venv/bin/python3 -m nitro_generator_checker
